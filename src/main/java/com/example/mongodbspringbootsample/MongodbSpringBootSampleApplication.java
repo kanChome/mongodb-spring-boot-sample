a3ecd69f2f694f41a396aa5f68a83e9fcf56a977
kanChome <study.hellbird@gmail.com>
2023-07-18 23:36:07 +0900

    Initial commit

---
+++ src/main/java/com/example/mongodbspringbootsample/MongodbSpringBootSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongodbspringbootsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbSpringBootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbSpringBootSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mongodbspringbootsample/MongodbSpringBootSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongodbspringbootsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbSpringBootSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
